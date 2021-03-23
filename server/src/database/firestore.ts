c9cffb12cfeb5f63a6edcc01cdf42db9f3326aae
Zaú Júlio <zauhdf@gmail.com>
2021-03-23 03:10:02 -0300

    Update firestore.ts

--- server/src/database/firestore.ts
+++ server/src/database/firestore.ts
@@ -4,7 +4,7 @@ require("dotenv").config();
 admin.initializeApp({
   credential: admin.credential.cert({
     projectId: process.env.PROJECT_ID,
-    privateKey: process.env.PRIVATE_KEY?.replace(/\\n/g, "\n"),
+    privateKey: process.env.PRIVATE_KEY?.replace(/\\n/g, "\n") || process.env.GCLOUD_CREDENTIALS,
     clientEmail: process.env.CLIENT_EMAIL,
   }),
   databaseURL: process.env.DATABASE_URL,
